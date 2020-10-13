e280ac0e70ed5595466130b3c9e51d897787b70e
NessMC <contact@nessmc.fr>
2020-10-13 18:15:14 +0200

    ✨ Formatting text process added.

--- src/tabdownParser/index.ts
+++ src/tabdownParser/index.ts
@@ -3,17 +3,19 @@ import { readFile } from "../readFile/index.ts";
 export class Parser {
   private readonly path: string;
   private content: string = '';
+  private formated: string[] = [];
 
   constructor(path: string) {
-
     this.path = path;
   }
 
   public async init() {
     this.content = await readFile(this.path);
-    console.log("init Parser");
+    this.formated = this.content
+      .split(/\r?\n/g)
+      .filter((x: string) => x.trim().length > 0);
+    console.log(this.formated);
   }
-
 }
 
 new Parser('./src/tabdownParser/code.txt').init();
