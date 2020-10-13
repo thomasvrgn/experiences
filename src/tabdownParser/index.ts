d93aa51c5e367ba38639b40d9b829b98435a685f
NessMC <contact@nessmc.fr>
2020-10-13 18:34:36 +0200

    ✨ Size tabs getter method added.

--- src/tabdownParser/index.ts
+++ src/tabdownParser/index.ts
@@ -4,7 +4,7 @@ import { Node } from "./typings/node.ts";
 export class Parser {
   private readonly path: string;
   private content: string = '';
-  private formated: string[] = [];
+  private formatted: string[] = [];
   private ast: Node = {
     type: 'Program',
     raw: '',
@@ -12,6 +12,15 @@ export class Parser {
     tabs: -1,
     children: [],
   };
+  private tabSize: number = 0;
+  private oldTabSize: number = -1;
+
+  private getSizeTabs(line: string): number {
+    if (!this.tabSize) return 0;
+    const matched: string[] = line.match(/^\s+/) || [''];
+    return matched[0].length / this.tabSize;
+  }
+
   private static parse: (code: string, index: number, ast: Node) => (typeof Parser.parse | null);
 
   constructor(path: string) {
@@ -20,13 +29,13 @@ export class Parser {
 
   public async init() {
     this.content = await readFile(this.path);
-    this.formated = this.content
+    this.formatted = this.content
       .split(/\r?\n/g)
       .filter((x: string) => x.trim().length > 0);
-    this.parse(this.formated, 0, this.ast);
+    this.parse(this.formatted, 0, this.ast);
   }
 
-  public parse(code: string[], index: number, ast: Node): typeof Parser.parse | Node {
+  private parse(code: string[], index: number, ast: Node): typeof Parser.parse | Node {
     const line: string = code[index];
     if (!line) return ast;
     console.log(line);
