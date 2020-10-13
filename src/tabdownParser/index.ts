01ea1fdc87f0b6112ba7772f1d007dc56e31c09f
NessMC <contact@nessmc.fr>
2020-10-13 18:24:01 +0200

    ✨ Parse method added.

--- src/tabdownParser/index.ts
+++ src/tabdownParser/index.ts
@@ -1,9 +1,18 @@
 import { readFile } from "../readFile/index.ts";
+import { Node } from "./typings/node.ts";
 
 export class Parser {
   private readonly path: string;
   private content: string = '';
   private formated: string[] = [];
+  private ast: Node = {
+    type: 'Program',
+    raw: '',
+    line: -1,
+    tabs: -1,
+    children: [],
+  };
+  private static parse: (code: string, index: number, ast: Node) => (typeof Parser.parse | null);
 
   constructor(path: string) {
     this.path = path;
@@ -14,7 +23,14 @@ export class Parser {
     this.formated = this.content
       .split(/\r?\n/g)
       .filter((x: string) => x.trim().length > 0);
-    console.log(this.formated);
+    this.parse(this.formated, 0, this.ast);
+  }
+
+  public parse(code: string[], index: number, ast: Node): typeof Parser.parse | Node {
+    const line: string = code[index];
+    if (!line) return ast;
+    console.log(line);
+    return this.parse(code, index + 1, ast);
   }
 }
 
